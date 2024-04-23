034d16e7baf67bc7aba40776af5bf4c415dc189e
hovanduan <hovanduanit@gmail.com>
2024-04-23 22:41:14 +0700

    x

---
+++ src/main/java/com/project/shopappbackend/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.project.shopappbackend.controller;
+
+import com.project.shopappbackend.dtos.CategoryDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/categories")
+public class CategoryController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAllCategories(@PathVariable("id") Long y,
+                                              @RequestParam(value = "name", required = false) String myName,
+                                              @RequestBody()CategoryDTO category, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY.value()).build();
+        }
+        return ResponseEntity.ok("ok " + y + " " + myName + category.getName());
+    }
+
+}
---
+++ src/main/java/com/project/shopappbackend/dtos/CategoryDTO.java
@@ -0,0 +1,10 @@
+package com.project.shopappbackend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+    private String name;
+}
